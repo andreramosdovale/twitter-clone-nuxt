d5a395be4a017ff36bb3e745300d9016cb623368
André Ramos <andre_ramosdovale@outlook.com>
2023-08-14 00:45:25 -0300

    fix: fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: '@typescript-eslint/parser',
+    parser: '@typescript-eslint/parser'
   },
-  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:@typescript-eslint/recommended',
-  'plugin:vue/vue3-essential',
-  'prettier',],
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential'
+  ],
   plugins: ['@typescript-eslint', 'vue'],
   rules: {
     'prefer-promise-reject-errors': 'off',
-    quotes: ['warn', 'single', { avoidEscape: true }],
     'vue/multi-word-component-names': 'off'
-  },
-};
+  }
+}
